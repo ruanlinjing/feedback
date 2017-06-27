9e55115c791e93c14698ce81ae19534980c029ed
umasuo <liuquan89@gmail.com>
2017-06-27 20:58:04 +0800

    init

--- src/main/java/com/umasuo/feedback/Application.java
+++ src/main/java/com/umasuo/feedback/Application.java
@@ -1,7 +1,5 @@
 package com.umasuo.feedback;
 
-import com.umasuo.report.infrastructure.config.DateConfig;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,7 +39,6 @@ public class Application {
    */
   @GetMapping("/")
   public String index() {
-    LOG.info("Zone: {}.", DateConfig.zoneId);
     return serviceName + ", system time: " + System.currentTimeMillis();
   }
 
