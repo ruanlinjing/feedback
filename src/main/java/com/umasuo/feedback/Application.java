da09778217bd2dc067a96e213c6ba6e265671779
umasuo <liuquan89@gmail.com>
2017-06-27 20:54:36 +0800

    init

---
+++ src/main/java/com/umasuo/feedback/Application.java
@@ -0,0 +1,52 @@
+package com.umasuo.feedback;
+
+import com.umasuo.report.infrastructure.config.DateConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by umasuo on 17/2/9.
+ */
+@SpringBootApplication(scanBasePackages = "com.umasuo")
+@EnableAutoConfiguration
+@Configuration
+@RestController
+@EnableScheduling
+public class Application {
+
+  /**
+   * Logger.
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+  /**
+   * service name.
+   */
+  @Value("${spring.application.name:feedback-service}")
+  private String serviceName;
+
+  /**
+   * This api is used for health check.
+   *
+   * @return service name.
+   */
+  @GetMapping("/")
+  public String index() {
+    LOG.info("Zone: {}.", DateConfig.zoneId);
+    return serviceName + ", system time: " + System.currentTimeMillis();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ src/main/java/com/umasuo/feedback/infrastructure/Router.java
@@ -0,0 +1,18 @@
+package com.umasuo.report.infrastructure;
+
+/**
+ * Created by umasuo on 17/6/11.
+ */
+public class Router {
+
+  /**
+   * report root.
+   */
+  public static final String FEEDBACK_ROOT = "/feedback";
+
+  /**
+   * The constant FEEDBACK_WITH_ID.
+   */
+  public static final String FEEDBACK_WITH_ID = FEEDBACK_ROOT + "/{id}";
+
+}
